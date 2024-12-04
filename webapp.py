15720785c7400202b5d0b45b4e5b2b01d2051d92
Jguan10 <jasoguan10@gmail.com>
2024-12-04 17:07:03 -0500

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -5,58 +5,89 @@ import re
 from sklearn.neighbors import NearestNeighbors
 import scipy.sparse
 import pickle
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+import nltk
+
+with st.spinner('Loading models...'):
+    with open('models/nearest_neighbors_model.pkl', 'rb') as f:
+        nearest_neighbors = pickle.load(f)
+    tfidf_matrix = scipy.sparse.load_npz('models/tfidf_matrix.npz')
+    with open('models/tfidf_vectorizer.pkl', 'rb') as f:
+        vectorizer = pickle.load(f)
 
 @st.cache_data
 def load_data():
-    return pd.read_csv('Data/recipes_food_com_cleaned.csv')
-
-@st.cache_resource
-def load_neighbors():
-    with open('models/nearest_neighbors_model.pkl', 'rb') as f:
-        nearest_neighbors = pickle.load(f)
-    return nearest_neighbors
-
-@st.cache_resource
-def load_matrix():
-    tfidf_matrix = scipy.sparse.load_npz('models/tfidf_matrix_updated.npz')
-    return tfidf_matrix
-
-@st.cache_resource
-def load_vectorizer():
-    with open('models/tfidf_vectorizer_updated.pkl', 'rb') as f:
-        vectorizer = pickle.load(f)
-    return vectorizer
-
-with st.spinner('Loading models...'):
-    nearest_neighbors = load_neighbors()
-    tfidf_matrix = load_matrix()
-    vectorizer = load_vectorizer()
+    data = pd.read_csv('Data/recipes_food_com_revised.csv')
+    return data
 
 with st.spinner('Loading data...'):
     data = load_data()
 
+lemmatizer = WordNetLemmatizer()
+
 @st.cache_data
-def recommend(ingredients_list, top_n=5):
-    user_vector = vectorizer.transform([ingredients_list])
+def lemmatize_string(string):
+    string_lower = string.lower()
+    tokens = word_tokenize(string_lower)
+    lemmatized_tokens = [lemmatizer.lemmatize(token) for token in tokens]
+    return ' '.join(lemmatized_tokens)
+
+@st.cache_data
+def lemmatize_list(list):
+    return [lemmatizer.lemmatize(item.lower()) for item in list]
+
+@st.cache_data
+def recommend(preferred_ingredients, top_n=5, excluded_ingredients=None):
+    excluded_ingredients = None if not excluded_ingredients.strip() else excluded_ingredients
+
+    if excluded_ingredients is None:
+        excluded_ingredients = []
+    elif isinstance(excluded_ingredients, str):
+        excluded_ingredients = excluded_ingredients.lower().split(', ')
+
+    lemmatized_ingredients = lemmatize_string(preferred_ingredients)
+
+    user_vector = vectorizer.transform([lemmatized_ingredients])
+    
     distances, indices = nearest_neighbors.kneighbors(user_vector, n_neighbors=top_n)
-    recommender = data.iloc[indices[0]].copy()
-    recommender['Similarity'] = 1 - distances[0]
-    return recommender
+    
+    recommendations = data.iloc[indices[0]].copy()
+    recommendations['Similarity'] = 1 - distances[0]  
+
+    lemmatized_excluded_ingredients = lemmatize_list(excluded_ingredients)
+    def contains_excluded(ingredients):
+        for excluded in lemmatized_excluded_ingredients:
+            if excluded in ingredients:
+                return True
+        return False
+    
+    recommendations = recommendations[~recommendations['NLP_Ingredients'].apply(contains_excluded)]
+    
+    return recommendations
 
 st.title('Dishcovery')
 ingredients_list = st.text_input("Which Ingredients Are You Using?")
+exclude_list = st.text_input("Any Allergies or Exceptions?")
+
 st.write(f'Your Ingredients List is: {ingredients_list}')
+st.write(f'Things to Exclude are: {exclude_list}')
 
 # Display each recipe in an expander
 if st.button('Get Recommendations', key = 'Recommendations'):
     with st.spinner('Recommending...'):
-        recommendations = recommend(ingredients_list)
+        recommendations = recommend(ingredients_list, excluded_ingredients = exclude_list)
         for index, row in recommendations.iterrows():
             with st.expander(row['Name']):
                 st.markdown(f"## {row['Name']}")
                 st.write(f"**Similarity:** {row['Similarity']:.2f}")
                 st.write("**Ingredients:**")
-                st.write(row['Cleaned_Ingredients'])
+                raw_ingredients_list = row['IngredientsRaw'].split("', '")
+                for i, step in enumerate(raw_ingredients_list, 1):
+                    step = step.replace("'", '').replace('(', '').replace(')', '')
+                    if not step.strip():  
+                        continue
+                    st.write(f"{i}. {step}")
 
                 # Display nutrition
                 st.write("**Nutrition**")
@@ -71,9 +102,12 @@ if st.button('Get Recommendations', key = 'Recommendations'):
                 st.write(f"**Protein:** {row['ProteinContent']:.2f}g")
                 
                 # Parse instructions into separate steps
-                instructions_list = row['Instructions'].split('", "')
                 st.write("**Instructions:**")
+                instructions_list = row['Instructions'].split("', '")
                 for i, step in enumerate(instructions_list, 1):
-                    step = step.replace('"', '').replace('[', '').replace(']', '')
+                    step = step.replace("'", '').replace('(', '').replace(')', '')
+                    if not step.strip():  
+                        continue
                     st.write(f"{i}. {step}")
                 st.write("------")
+
