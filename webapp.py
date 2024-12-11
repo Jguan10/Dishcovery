679647b73d8f35344d31fbf5ce2908ecc000e1a8
Jguan10 <jasoguan10@gmail.com>
2024-12-11 16:06:57 -0500

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -11,6 +11,20 @@ from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
 import zstandard as zstd
 
+def update_slider():
+    st.session_state.slider = st.session_state.numeric
+def update_numin():
+    st.session_state.numeric = st.session_state.slider            
+
+val = st.number_input('Input', value = 0, key = 'numeric', on_change = update_slider)
+
+
+slider_value = st.slider('slider', min_value = 0, 
+                        value = val, 
+                        max_value = 5,
+                        step = 1,
+                        key = 'slider', on_change= update_numin)
+
 def get_memory_usage():
     process = psutil.Process()
     memory_info = process.memory_info()
