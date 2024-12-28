fd5c45afacf1c9525983f483e9517833c3a0e8c9
Jguan10 <jasoguan10@gmail.com>
2024-12-28 01:18:57 -0500

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -111,8 +111,7 @@ def recommend(preferred_ingredients, top_n=5, excluded_ingredients=None):
 
 st.title('Dishcovery')
 url = "https://github.com/Jguan10/Dishcovery"
-st.write("check out this [link](%s)" % url)
-st.markdown("check out this [link](%s)" % url)
+st.markdown("Check out the Github [here](%s)!" % url)
 ingredients_list = st.text_input("Which Ingredients Are You Using?")
 exclude_list = st.text_input("Any Allergies or Exceptions?")
 
